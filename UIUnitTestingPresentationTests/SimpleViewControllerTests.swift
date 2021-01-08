ed0f414ec89a8ef3c578291e7f3e5bd2f05c8105
Yannic Borgfeld <yannic.borgfeld@check24.de>
2021-01-08 11:16:07 +0100

    Extract duplicated system under test (SUT) creation logic into shared helper method

--- UIUnitTestingPresentationTests/SimpleViewControllerTests.swift
+++ UIUnitTestingPresentationTests/SimpleViewControllerTests.swift
@@ -13,20 +13,23 @@ class SimpleViewControllerTests: XCTestCase {
     // sut = system under test
     
     func test_button_isConnected() {
-        let bundle = Bundle(for: SimpleViewController.self)
-        let sb = UIStoryboard(name: "Simple", bundle: bundle)
-        let sut: SimpleViewController = sb.instantiateViewController(identifier: String(describing: SimpleViewController.self))
+        let sut = makeSUT()
         sut.loadViewIfNeeded()
         
         XCTAssertNotNil(sut.button)
     }
     
     func test_textLabel_isConnected() {
-        let bundle = Bundle(for: SimpleViewController.self)
-        let sb = UIStoryboard(name: "Simple", bundle: bundle)
-        let sut: SimpleViewController = sb.instantiateViewController(identifier: String(describing: SimpleViewController.self))
+        let sut = makeSUT()
         sut.loadViewIfNeeded()
 
         XCTAssertNotNil(sut.textLabel)
+    }
+        
+    private func makeSUT() -> SimpleViewController {
+        let bundle = Bundle(for: SimpleViewController.self)
+        let sb = UIStoryboard(name: "Simple", bundle: bundle)
+        let sut: SimpleViewController = sb.instantiateViewController(identifier: String(describing: SimpleViewController.self))
+        return sut
     }
 }
