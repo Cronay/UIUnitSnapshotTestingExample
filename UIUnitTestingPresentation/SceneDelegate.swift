c439cae1f7651b9535822c106a1645031afee3bd
Yannic Borgfeld <yannic.borgfeld@check24.de>
2021-01-08 11:58:08 +0100

    Compose view controller with view model and loader in scene delegate

--- UIUnitTestingPresentation/SceneDelegate.swift
+++ UIUnitTestingPresentation/SceneDelegate.swift
@@ -11,12 +11,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let bundle = Bundle(for: SimpleViewController.self)
+        let sb = UIStoryboard(name: "Simple", bundle: bundle)
+        let sut: SimpleViewController = sb.instantiateViewController(identifier: String(describing: SimpleViewController.self))
+        let loader = Loader()
+        sut.viewModel = SimpleViewModel(loader: loader)
+        window?.rootViewController = sut
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +53,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
+class Loader: TextLoader {
+    func load(completion: @escaping (String) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            completion("Check24")
+        }
+    }
+}
