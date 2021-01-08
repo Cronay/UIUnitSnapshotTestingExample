c0f374725dae1d7d5beecfdfa16030a21d25bba4
Yannic Borgfeld <yannic.borgfeld@check24.de>
2021-01-08 10:56:57 +0100

    Set up initial demo project

---
+++ UIUnitTestingPresentation/SimpleViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SimpleViewController.swift
+//  UIUnitTestingPresentation
+//
+//  Created by Yannic Borgfeld on 08.01.21.
+//
+
+import UIKit
+
+class SimpleViewController: UIViewController {
+    
+}
---
+++ UIUnitTestingPresentationTests/SimpleViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  SimpleViewControllerTests.swift
+//  UIUnitTestingPresentationTests
+//
+//  Created by Yannic Borgfeld on 08.01.21.
+//
+
+import XCTest
+
+class SimpleViewControllerTests: XCTestCase {
+
+}
